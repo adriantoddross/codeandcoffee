20b9cc0e6fa6d34b8fdfe4506618edd0563ef6e8
Adrian Ross <adriantoddross@gmail.com>
2021-05-06 20:44:32 -0400

    Init remove duplicates challenge

---
+++ weekly-exercises/remove-duplicates/README.md
@@ -0,0 +1,6 @@
+## Remove Duplicates
+
+ Write a function `removeDuplicates` that removes duplicates characters from a string.
+
+ See [index.js](https://replit.com/@BiancaGandolfo/remove-duplicates#index.js) for code and test cases.
+
---
+++ weekly-exercises/remove-duplicates/index.js
@@ -0,0 +1,12 @@
+/*
+* Removes duplicates characters from a string
+* @param {string} str - the string
+* @return {string} - a string without duplicates
+*/
+
+function removeDuplicates(str) {
+ // TODO
+}
+
+// make this true
+console.log(removeDuplicates('mississippi') === 'misp');
