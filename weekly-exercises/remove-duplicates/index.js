81a6e2ef1efd4d5765f01f4ee69a9d80d5b999fb
Adrian Ross <adriantoddross@gmail.com>
2021-05-07 11:28:58 -0400

    Pseudocode ideas for solutions

--- weekly-exercises/remove-duplicates/index.js
+++ weekly-exercises/remove-duplicates/index.js
@@ -1,12 +1,17 @@
 /*
-* Removes duplicates characters from a string
-* @param {string} str - the string
-* @return {string} - a string without duplicates
-*/
+ * Removes duplicates characters from a string
+ * @param {string} str - the string
+ * @return {string} - a string without duplicates
+ */
 
 function removeDuplicates(str) {
- // TODO
+  /*
+  Approaches:
+  - For loop: add unique chars to a new string, omit recurring/duplicate chars
+  - Use an array method to .reduce a string down to unique chars
+  - Map all letters and counts to an object, then return all object keys
+ */
 }
 
 // make this true
-console.log(removeDuplicates('mississippi') === 'misp');
+console.log(removeDuplicates("mississippi") === "misp");
